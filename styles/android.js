b9025f462caef36a0fc15f73bbffb72af49052a5
Dominic Serrano <dmsrojo@gmail.com>
2017-01-21 15:39:02 -0600

    Exported styles to seperate dir and now importing in android.index.js

---
+++ styles/android.js
@@ -0,0 +1,29 @@
+'use strict';
+
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 100,
+    height:200,
+    marginLeft: 10
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  year: {
+    textAlign: 'center'
+  }
+});
