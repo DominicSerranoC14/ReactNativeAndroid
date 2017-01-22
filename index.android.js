6d717243fae6e4be013797b49e31d40f36ec060e
Dominic Serrano <dmsrojo@gmail.com>
2017-01-22 15:52:12 -0600

    Added minor styles and fixed N/A image urls

--- index.android.js
+++ index.android.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View, Button, Image } from 'react-native';
+import { AppRegistry, Text, TextInput, View, Button, Image, ScrollView } from 'react-native';
 
 export default class ReactNativeAndroid extends Component {
 
@@ -25,8 +25,10 @@ export default class ReactNativeAndroid extends Component {
     fetch(`http://www.omdbapi.com/?s=${this.state.value}`)
     .then(response => response.json())
     .then(jsonObj => jsonObj.Search)
-    .then(movieArray => this.setState({ movieSearch: movieArray, movieState: true }))
-    // .then(data => console.log("Data type: ", typeof data))
+    .then(movieArray => this.setState({
+      movieSearch: movieArray,
+      movieState: true
+    }))
     .catch(console.error);
 
   };
@@ -35,20 +37,28 @@ export default class ReactNativeAndroid extends Component {
 
     if (this.state.movieState) {
       return (
-        <View >
-          <Text style={{fontSize: 25, textAlign: 'center', fontFamily: 'Times New Roman'}}>Search Results</Text>
+        <ScrollView >
+          <Text style={{fontSize: 30, textAlign: 'center', fontFamily: 'Times New Roman', margin: 10}}>Search Results</Text>
+
           {this.state.movieSearch.map((each, i) => {
+            if (each.Poster === 'N/A') {
+              each.Poster = 'http://vignette2.wikia.nocookie.net/main-cast/images/5/5b/Sorry-image-not-available.png/revision/latest?cb=20160625173435';
+            };
+
             return (
+
               <View key={i} style={{flexDirection: 'row', backgroundColor: 'lightgrey', alignItems: 'center', padding: 10, marginBottom: 10}}>
+
                 <Image style={{width: 150, height: 175}} source={{uri: each.Poster}} />
                 <View style={{flex: 1}}>
-                  <Text style={{fontSize: 25, textAlign: 'center', fontFamily: 'Times New Roman'}} >{each.Title}</Text>
-                  <Text style={{fontSize: 15, textAlign: 'center', fontFamily: 'Times New Roman'}} >{each.Year}</Text>
+                  <Text style={{fontSize: 25, textAlign: 'center', fontFamily: 'Times New Roman', color: 'white'}} >{each.Title}</Text>
+                  <Text style={{fontSize: 15, textAlign: 'center', fontFamily: 'Times New Roman', color: 'white'}} >{each.Year}</Text>
                 </View>
+
               </View>
-            )
+            );
           })}
-        </View>
+        </ScrollView>
       )
     };
 
