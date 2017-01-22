7f9dfb79c45dd28d95e22a2fc9cd3106ff13b595
Dominic Serrano <dmsrojo@gmail.com>
2017-01-22 12:50:00 -0600

    Trying to render the movieArray that is returned from fetch

--- index.android.js
+++ index.android.js
@@ -9,7 +9,8 @@ export default class ReactNativeAndroid extends Component {
     super(props);
     this.state = {
       value: '',
-      submitValue: ''
+      submitValue: '',
+      movieArray: ''
     };
 
     // Must bind function to this if you want use this
@@ -17,34 +18,58 @@ export default class ReactNativeAndroid extends Component {
   };
 
   onButtonPress() {
-    this.setState({submitValue: this.state.value})
+    console.log(this.state.value);
+    alert(this.state.value);
+
+    fetch(`http://www.omdbapi.com/?s=${this.state.value}`)
+    .then(response => response.json())
+    .then(jsonObj => jsonObj.Search)
+    .then(movieArray => this.setState({movieSearch: movieArray}))
+    .catch(console.error);
+
   };
 
   render() {
 
     return (
-      <View style={{flex: 1}}>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={{fontSize: 30, fontFamily: 'Times New Roman'}}>Movie Finder</Text>
         <TextInput
-          style={{height: 40, width: 100}}
+          style={{height: 40, width: 200, margin: 20}}
           placeholder="Type here"
           value={this.state.value}
           onChangeText={text => this.setState({value: text})}
-          onSubmitEditing={() => this.setState({ submitValue : this.state.value })}
+          onSubmitEditing={this.onButtonPress}
         />
 
-        <Text style={{height: 40, width: 100}} >{this.state.value}</Text>
-        <Text style={{height: 40, width: 100}} >{this.state.submitValue}</Text>
-
-        <Button
-          containerStyle={{padding:10, height:45, width: 100}}
-          onPress={this.onButtonPress}
-          title="Learn More"
-          color="#841584"
-        />
+        <View style={{width: 200}}>
+          <Button
+            onPress={this.onButtonPress}
+            title="Search"
+            color="green"
+          />
+        </View>
 
       </View>
     );
   }
+}
+
+
+class MovieContent extends Component {
+
+  constructor(props) {
+    super(props);
+  };
+
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        {this.props.movieArray.map((each, i) => <Text key={i}>{each.Title}</Text>)}
+      </View>
+    )
+  };
+
 }
 
 
