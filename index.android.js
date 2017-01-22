57f3f67b9a2bbb4f56612e6cf85511652351d96f
Dominic Serrano <dmsrojo@gmail.com>
2017-01-22 12:07:13 -0600

    Now able to access input value on button press

--- index.android.js
+++ index.android.js
@@ -1,18 +1,27 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
+import { AppRegistry, Text, TextInput, View, Button } from 'react-native';
 
 export default class ReactNativeAndroid extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value: '',
+      submitValue: ''
     };
+
+    // Must bind function to this if you want use this
+    this.onButtonPress = this.onButtonPress.bind(this);
+  };
+
+  onButtonPress() {
+    this.setState({submitValue: this.state.value})
   };
 
   render() {
+
     return (
       <View style={{flex: 1}}>
         <TextInput
@@ -20,8 +29,19 @@ export default class ReactNativeAndroid extends Component {
           placeholder="Type here"
           value={this.state.value}
           onChangeText={text => this.setState({value: text})}
+          onSubmitEditing={() => this.setState({ submitValue : this.state.value })}
         />
+
         <Text style={{height: 40, width: 100}} >{this.state.value}</Text>
+        <Text style={{height: 40, width: 100}} >{this.state.submitValue}</Text>
+
+        <Button
+          containerStyle={{padding:10, height:45, width: 100}}
+          onPress={this.onButtonPress}
+          title="Learn More"
+          color="#841584"
+        />
+
       </View>
     );
   }
