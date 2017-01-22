03f287193f1f89d00c51665270108d17818283fc
Dominic Serrano <dmsrojo@gmail.com>
2017-01-22 15:39:47 -0600

    Now rendering search results from omdb

--- index.android.js
+++ index.android.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View, Button } from 'react-native';
+import { AppRegistry, Text, TextInput, View, Button, Image } from 'react-native';
 
 export default class ReactNativeAndroid extends Component {
 
@@ -10,7 +10,8 @@ export default class ReactNativeAndroid extends Component {
     this.state = {
       value: '',
       submitValue: '',
-      movieArray: ''
+      movieSearch: null,
+      movieState: false
     };
 
     // Must bind function to this if you want use this
@@ -24,15 +25,36 @@ export default class ReactNativeAndroid extends Component {
     fetch(`http://www.omdbapi.com/?s=${this.state.value}`)
     .then(response => response.json())
     .then(jsonObj => jsonObj.Search)
-    .then(movieArray => this.setState({movieSearch: movieArray}))
+    .then(movieArray => this.setState({ movieSearch: movieArray, movieState: true }))
+    // .then(data => console.log("Data type: ", typeof data))
     .catch(console.error);
 
   };
 
   render() {
 
+    if (this.state.movieState) {
+      return (
+        <View >
+          <Text style={{fontSize: 25, textAlign: 'center', fontFamily: 'Times New Roman'}}>Search Results</Text>
+          {this.state.movieSearch.map((each, i) => {
+            return (
+              <View key={i} style={{flexDirection: 'row', backgroundColor: 'lightgrey', alignItems: 'center', padding: 10, marginBottom: 10}}>
+                <Image style={{width: 150, height: 175}} source={{uri: each.Poster}} />
+                <View style={{flex: 1}}>
+                  <Text style={{fontSize: 25, textAlign: 'center', fontFamily: 'Times New Roman'}} >{each.Title}</Text>
+                  <Text style={{fontSize: 15, textAlign: 'center', fontFamily: 'Times New Roman'}} >{each.Year}</Text>
+                </View>
+              </View>
+            )
+          })}
+        </View>
+      )
+    };
+
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+
         <Text style={{fontSize: 30, fontFamily: 'Times New Roman'}}>Movie Finder</Text>
         <TextInput
           style={{height: 40, width: 200, margin: 20}}
@@ -53,23 +75,6 @@ export default class ReactNativeAndroid extends Component {
       </View>
     );
   }
-}
-
-
-class MovieContent extends Component {
-
-  constructor(props) {
-    super(props);
-  };
-
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        {this.props.movieArray.map((each, i) => <Text key={i}>{each.Title}</Text>)}
-      </View>
-    )
-  };
-
 }
 
 
