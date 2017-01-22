1d18aef917649322ff16ab52186e58aac26eab23
Dominic Serrano <dmsrojo@gmail.com>
2017-01-22 16:04:37 -0600

    Added back button functionality and more styles to search page

--- index.android.js
+++ index.android.js
@@ -15,13 +15,10 @@ export default class ReactNativeAndroid extends Component {
     };
 
     // Must bind function to this if you want use this
-    this.onButtonPress = this.onButtonPress.bind(this);
+    this.searchForMovieTitle = this.searchForMovieTitle.bind(this);
   };
 
-  onButtonPress() {
-    console.log(this.state.value);
-    alert(this.state.value);
-
+  searchForMovieTitle() {
     fetch(`http://www.omdbapi.com/?s=${this.state.value}`)
     .then(response => response.json())
     .then(jsonObj => jsonObj.Search)
@@ -30,7 +27,6 @@ export default class ReactNativeAndroid extends Component {
       movieState: true
     }))
     .catch(console.error);
-
   };
 
   render() {
@@ -38,16 +34,25 @@ export default class ReactNativeAndroid extends Component {
     if (this.state.movieState) {
       return (
         <ScrollView >
-          <Text style={{fontSize: 30, textAlign: 'center', fontFamily: 'Times New Roman', margin: 10}}>Search Results</Text>
+          <View style={{flexDirection: 'row', justifyContent: 'flex-start', marginTop: 10}}>
+            <Button
+              onPress={() => this.setState({movieState: false, value: ''})}
+              title="Back"
+              color="red"
+            />
+            <Text style={{fontSize: 30, fontFamily: 'Times New Roman', textAlign: 'center', marginLeft: 40}}>Search Results</Text>
+          </View>
+
 
           {this.state.movieSearch.map((each, i) => {
+
             if (each.Poster === 'N/A') {
               each.Poster = 'http://vignette2.wikia.nocookie.net/main-cast/images/5/5b/Sorry-image-not-available.png/revision/latest?cb=20160625173435';
             };
 
             return (
 
-              <View key={i} style={{flexDirection: 'row', backgroundColor: 'lightgrey', alignItems: 'center', padding: 10, marginBottom: 10}}>
+              <View key={i} style={{flexDirection: 'row', backgroundColor: 'lightgrey', alignItems: 'center', padding: 10, marginTop: 10}}>
 
                 <Image style={{width: 150, height: 175}} source={{uri: each.Poster}} />
                 <View style={{flex: 1}}>
@@ -71,12 +76,12 @@ export default class ReactNativeAndroid extends Component {
           placeholder="Type here"
           value={this.state.value}
           onChangeText={text => this.setState({value: text})}
-          onSubmitEditing={this.onButtonPress}
+          onSubmitEditing={this.searchForMovieTitle}
         />
 
         <View style={{width: 200}}>
           <Button
-            onPress={this.onButtonPress}
+            onPress={this.searchForMovieTitle}
             title="Search"
             color="green"
           />
